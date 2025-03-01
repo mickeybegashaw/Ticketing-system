c9f80907034d342af5a2981a93dacb73c9c180b3
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-01 23:25:10 +0300

    update server.js to import and use ticket routes

--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import authRoutes from "./routes/auth.js";
-// const authRoutes = require("./routes/authRoutes");
-// const ticketRoutes = require("./routes/ticketRoutes");
+import ticketRoute from './routes/tickets.js'
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/tickets", ticketRoutes);
+app.use("/api/tickets", ticketRoute);
 
 mongoose.connect(process.env.MONGODB_URI).then(
   app.listen(process.env.PORT, () => {
