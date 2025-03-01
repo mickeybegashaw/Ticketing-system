cdfeabcd26b6ca49fd9ecedceabb75b8fba9186f
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-01 22:49:01 +0300

    add user model with schema

---
+++ Backend/models/User.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
+
+module.exports = mongoose.model("User", userSchema);
