2e08edf42374d1748238a3636123b1848db9240f
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-02 22:58:12 +0300

    feat: add Footer component to RootLayout for improved layout structure

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-full h-20 flex justify-center items-center">
+      <p>&copy; {new Date().getFullYear()} Developed by <a href="https://mikiyas-begashaw.vercel.app">Mikiya Begashaw</a></p>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/layout/RootLayout.jsx
+++ frontend/src/layout/RootLayout.jsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 
 const RootLayout = ({children}) => {
@@ -5,6 +6,7 @@ const RootLayout = ({children}) => {
     <>
       <Header/>
       {children}
+      <Footer/>
     </>
   )
 }
