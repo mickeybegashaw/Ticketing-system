503981c69f50a43c9e1f99b08bef1ade662716fb
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-03-03 14:02:07 +0300

    feat: implement role-based routing with user and admin dashboards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,30 @@
 import Home from "./pages/home";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
+import UserDashboard from "./pages/UserDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import UseAuth from "./hooks/useAuth";
 
 const App = () => {
+  const { user } = UseAuth();
+  console.log(user);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<SignUp/>} />
+        
+        {/* Public Routes */}
+        <Route 
+          path="/login" 
+          element={user ? (user.role === "admin" ? <Navigate to="/admin" /> : <Navigate to="/user" />) : <Login />} 
+        />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Protected Routes */}
+        <Route path="/user" element={user && user.role === "user" ? <UserDashboard /> : <Navigate to="/login" />} />
+        <Route path="/admin" element={user && user.role === "admin" ? <AdminDashboard /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   );
